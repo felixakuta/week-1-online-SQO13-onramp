2ddbbaad83235f71c88be9bd5e03082b5859115d
unknown <felixezeugo@gmail.com>
2022-09-10 19:22:23 +0100

    third commit

--- algorithms/challenge-3.js
+++ algorithms/challenge-3.js
@@ -1,5 +1,12 @@
 function sumMix(arr) {
-   
- }
+    let sum = 0;
+    
+    let numOnlyArr = arr.map((item) => { return parseInt(item, 10);});
+    numOnlyArr.map((item) => { return sum += item; })
+        return sum;
+    }
+    
 
-  module.exports = sumMix
+    module.exports = sumMix
+    
+    console.log(sumMix([9, 3, '7', '3']));
