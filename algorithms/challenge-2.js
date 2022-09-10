8d461bc1db71b0ee9717128f20e3b8a279694596
unknown <felixezeugo@gmail.com>
2022-09-10 18:39:30 +0100

    second commit

--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,7 +1,17 @@
 function fizzBuzz(input){
-
+    if (input % 3 == 0 && input % 5) {
+      return "fizzbuzz"
+    } else if (input % 3 == 0) {
+        return"fizz"
+    } else if (input % 5 == 0) {
+        return "buzz"
+    } 
+  }
   
-}
-
-
-module.exports = fizzBuzz
+console.log(fizzBuzz((3)));
+console.log(fizzBuzz((5)));
+console.log(fizzBuzz((15)));
+console.log(fizzBuzz((7)));
+// console.log(fizzBuzz([4, "z", "f", 5]);
+// console.log(fizzBuzz(["$%^", 567, "&&&"]);
+// module.exports = fizzBuzz
