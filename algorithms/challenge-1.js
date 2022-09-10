8614e8f1db36755421b147f67b54e7728d90b017
unknown <felixezeugo@gmail.com>
2022-09-10 18:15:34 +0100

    first commit

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,4 +1,14 @@
-function countTruthy(arr) {
-
+function countTruthy(arr){
+    let score = []
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] > 0) {
+            score.push(arr[i])
+        }
+    }return score.length
+  
 }
-module.exports = countTruthy
+
+console.log(countTruthy([6, 3, 0, 30, 7]));
+console.log(countTruthy(['', 3, 0, 30, 7]));
+console.log(countTruthy(['', 0, 0, undefined, 7]));
+module.exports = countTruthy;
