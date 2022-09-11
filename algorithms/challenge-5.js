41638ce7e6a202a52505c85da64517ddb840242c
unknown <felixezeugo@gmail.com>
2022-09-11 12:10:41 +0100

    fifth commit

--- algorithms/challenge-5.js
+++ algorithms/challenge-5.js
@@ -1,5 +1,8 @@
 function filterList(arr) {
- 
+    return arr.filter(function (item) { return typeof item === 'number' });
  }
 
-  module.exports =filterList
+module.exports = filterList
+console.log(filterList([1, 2, 'a', 'b']));
+console.log(filterList([1,'a','b',0,15]));
+console.log(filterList([1,2,'aasf','1','123',123]));
