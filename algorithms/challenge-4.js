8d807ad527260099f2faa5cff47b8eaf9bdadb45
unknown <felixezeugo@gmail.com>
2022-09-11 11:57:52 +0100

    fourth commit

--- algorithms/challenge-4.js
+++ algorithms/challenge-4.js
@@ -1,4 +1,22 @@
 function calculateGrade(marks) {
- 
- }
+    if (marks<=49) {
+        return "F";
+    } else if (marks <= 59) {
+        return "E";
+    } else if (marks<=69) {
+        return "D";
+    } else if (marks <= 79) {
+        return "C";
+    } else if (marks <= 89) {
+        return "B";
+    } else if (marks <= 100) {
+        return "A";
+    } else {
+        return "Score not available";
+    }
+}
+
+   
   module.exports =calculateGrade
+    console.log(calculateGrade([34]));
+    
